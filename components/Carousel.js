4c80036dea6baba8b7f10ee8953b23a3bb7b1062
tarmes <traarmes@gmail.com>
2020-09-04 13:14:13 -0400

    mvp

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,62 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function MakeCarousel() {
+
+  const carousel = document.createElement('div')
+  const leftButton = document.createElement('div')
+  const mountainPic = document.createElement('img')
+  const computerPic = document.createElement('img')
+  const treesPic = document.createElement('img')
+  const turntablePic = document.createElement('img')
+  const rightButton = document.createElement('div')
+
+  carousel.appendChild(leftButton)
+  
+  carousel.appendChild(rightButton)
+
+  carousel.classList.add('carousel')
+  leftButton.classList.add('left-button')
+  rightButton.classList.add('right-button')
+  mountainPic.src = 'https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg'
+  computerPic.src = 'https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg'
+  treesPic.src = 'https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg'
+  turntablePic.src = 'https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg'
+  
+  let index = 0
+
+  leftButton.addEventListener('click', evt => {
+    index = index - 1
+    console.log(index)
+  })
+
+  rightButton.addEventListener('click', evt => {
+    index = index + 1
+    console.log(index)
+  })
+
+  // carousel.appendChild(mountainPic)
+  // carousel.appendChild(computerPic)
+  // carousel.appendChild(treesPic)
+  // carousel.appendChild(turntablePic)
+
+  if (index === 0) {
+    carousel.appendChild(mountainPic)
+  } else if (index === 1) {
+    carousel.appendChild(computerPic)
+  } else if (index === 2) {
+    carousel.appendChild(treesPic)
+  } else if (index ===3) {
+    carousel.appendChild(turntablePic)
+  }
+
+  return carousel
+}
+
+
+const carouselContainer = document.querySelector('.carousel-container')
+
+
+carouselContainer.appendChild(MakeCarousel())
+
